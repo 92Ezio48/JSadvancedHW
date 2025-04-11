dde1b7a4be4222315ee6e5ba998275cd81820d25
92Ezio48 <vovanew77714@gmail.com>
2025-04-11 18:23:16 +0300

    complete full hw-4

--- createCommentBtn.js
+++ createCommentBtn.js
@@ -1,28 +1,28 @@
-import { nameEl } from "./index.js";
-import { commentEl } from "./index.js";
-import { buttonEl } from "./index.js";
-import { comments } from "./commentsData.js";
-import { replaceAllFunction } from "./replaceAllFunction.js";
-import { currentDate } from "./index.js";
-import { renderComments } from "./renderCommentsFunction.js";
-buttonEl.addEventListener("click", () => {
-  if (nameEl.value === "") {
-    alert("Имя пользователя не введено!");
-    return;
-  }
-  if (commentEl.value === "") {
-    alert("Комментарий не введен!");
-    return;
-  }
+import { nameEl } from './renderCommentsFunction.js'
+import { commentEl } from './renderCommentsFunction.js'
+import { buttonEl } from './renderCommentsFunction.js'
+import { comments } from './commentsData.js'
+import { replaceAllFunction } from './replaceAllFunction.js'
+import { currentDate } from './renderCommentsFunction.js'
+import { renderComments } from './renderCommentsFunction.js'
+buttonEl.addEventListener('click', () => {
+    if (nameEl.value === '') {
+        alert('Имя пользователя не введено!')
+        return
+    }
+    if (commentEl.value === '') {
+        alert('Комментарий не введен!')
+        return
+    }
 
-  comments.push({
-    name: replaceAllFunction(nameEl.value),
-    time: currentDate,
-    text: replaceAllFunction(commentEl.value),
-    likesCounter: 0,
-    likeActive: false,
-  });
-  nameEl.value = "";
-  commentEl.value = "";
-  renderComments();
-});
+    comments.push({
+        name: replaceAllFunction(nameEl.value),
+        time: currentDate,
+        text: replaceAllFunction(commentEl.value),
+        likesCounter: 0,
+        likeActive: false,
+    })
+    nameEl.value = ''
+    commentEl.value = ''
+    renderComments()
+})
--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 "use strict";
 import { renderComments } from "./renderCommentsFunction.js";
-export const nameEl = document.querySelector(".add-form-name");
-export const commentEl = document.querySelector(".add-form-text");
-export const buttonEl = document.querySelector(".add-form-button");
-export const list = document.querySelector(".comments");
-export let today = new Date();
-export let day = String(today.getDate()).padStart(2, "0");
-export let month = String(today.getMonth() + 1).padStart(2, "0");
-export let year = today.getFullYear().toString().substr(2, 2);
-export let hours = String(today.getHours()).padStart(2, "0");
-export let minutes = String(today.getMinutes()).padStart(2, "0");
-export let currentDate = `${day}.${month}.${year} ${hours}:${minutes}`;
-export const commentFormsEl = document.querySelectorAll(".comment");
+
 
 renderComments();
